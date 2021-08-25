dfeb6ee4b678173eb7ca26667b1b90c444f769e2
AshleshaYeolee <ashleshayeole11@gmail.com>
2021-08-25 22:33:44 +0530

    Initial commit

---
+++ src/Product.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import "./Product.css";
+
+
+function Product() {
+    return (
+        <div
+          clasName="product">
+            <div className="product__info">
+                <p>The lean startup</p>
+                <p className="product__price">
+                   <small>$</small>
+                   <strong>19.99</strong>
+                </p>
+                <div className="product__rating">
+                  <p>🌟</p>
+                </div>
+            </div>
+
+            <img
+               src=""
+               alt=""
+               />
+
+            <button>Add to Basket</button>
+            
+        </div>
+    )
+}
+
+export default Product;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBE4bFUV3h1kEbL9i3NOm7mFJCvC_J3MAw",
+    authDomain: "challenge-28254.firebaseapp.com",
+    projectId: "challenge-28254",
+    storageBucket: "challenge-28254.appspot.com",
+    messagingSenderId: "1013374278291",
+    appId: "1:1013374278291:web:f3f2a66cdebabd2806cb66",
+    measurementId: "G-TCKRFYCXQD"
+  };
